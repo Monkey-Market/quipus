e0424de1e632091e43e0d834127a4e13ba2aac6d
J1Loop <j1dev@proton.me>
2024-10-06 21:50:39 +0200

    [FT] Add PostgreSQL data source

--- src/main.py
+++ src/main.py
@@ -1,36 +1,38 @@
-import os
-from models.template import Template
-from services.template_manager import TemplateManager
-from services.s3_delivery import AWSConfig, S3Delivery
+from data_sources.postgresql_data_source import PostgreSQLDataSource
 
 
 def main():
-    (
-        TemplateManager()
-        .from_source("csv", path_to_file="data/certificates.csv")
-        .with_multiple_templates(
-            [
-                Template(html_path="templates/template_es.html"),
-                Template(html_path="templates/template_en.html"),
-            ]
-        )
-        .decide_template_with(lambda item: f"templates/template_{item["lang"]}.html")
-        .decide_filename_with(lambda item: f"{item["name"]}_{item["content"]}")
-        .to_pdf("output", create_dir=True)
+    postgres_source = PostgreSQLDataSource(
+        host="<HOSTNAME>",
+        database="<DATABASE>",
+        user="<USERNAME>",
+        password="<PASSWORD>",
+        port=5432,
     )
+
+    try:
+        # First query - Se crea un pool de conexiones
+        query1 = "SELECT * FROM table1;"
+        data1, columns1 = postgres_source.fetch_data(query1)
+        print("Datos obtenidos de table1:\n")
+        print(columns1)
+        for row in data1:
+            print(row)
+
+        # Segunda consulta - Se reutiliza el pool de conexiones
+        query2 = "SELECT * FROM table2;"
+        data2, columns2 = postgres_source.fetch_data(query2)
+        print("\nDatos obtenidos de table2:\n")
+        print(columns2)
+        for row in data2:
+            print(row)
+
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+
+    finally:
+        postgres_source.close_pool()
+
 
 if __name__ == "__main__":
     main()
-
-    mytuple = []
-    for file in os.walk("output/"):
-        for f in file[2]:
-            mytuple.append((f"output/{f}", f))
-
-    print("Uploading files to S3...")
-
-    S3Delivery(AWSConfig.from_profile()).upload_many_files(
-        files=mytuple,
-        bucket_name="airbyte-storage-s3",
-    )
-
