f6452f8117b6a66b570cdc4b16a99640ce532ee4
J1Loop <j1dev@proton.me>
2024-11-01 22:35:08 +0100

    [FIX] Remove unused files & imports

--- quipus/__init__.py
+++ quipus/__init__.py
@@ -1,16 +1,14 @@
 """
 The root module of the library provides a unified API for accessing the main 
 components of the library. It includes functionalities for loading data from 
-various sources, managing documents (such as certificates), and delivering 
+various sources, managing documents (such as invoices), and delivering 
 files or documents through email, SFTP, or S3.
 
 Classes:
     CSVDataSource: Loads data from CSV files.
-    DataFrameDataSource: Loads data from pandas DataFrames.
+    DataFrameDataSource: Loads data from polars DataFrames.
     PostgreSQLDataSource: Loads data from PostgreSQL databases.
     XLSXDataSource: Loads data from XLSX files.
-    Certificate: Represents a certificate entity.
-    CertificateFactory: Provides methods for creating certificates.
     Template: Manages HTML templates with associated CSS and assets.
     EmailMessageBuilder: Helps in constructing email messages.
     EmailSender: Sends emails via an SMTP server.
@@ -26,7 +24,7 @@ from .data_sources import (
     PostgreSQLDataSource,
     XLSXDataSource,
 )
-from .models import Certificate, CertificateFactory, Template
+from .models import Template
 from .services import (
     EmailMessageBuilder,
     EmailSender,
@@ -42,8 +40,6 @@ __all__ = [
     "DataFrameDataSource",
     "PostgreSQLDataSource",
     "XLSXDataSource",
-    "Certificate",
-    "CertificateFactory",
     "Template",
     "EmailMessageBuilder",
     "EmailSender",
--- quipus/models/__init__.py
+++ quipus/models/__init__.py
@@ -1,17 +1,13 @@
 """
 The `models` module defines the core data models for the application, 
-including certificates and templates. These models represent the core entities 
+including templates. These models represent the core entities 
 used across the system and are essential for generating, managing, and 
-customizing documents like certificates.
+customizing documents like invoices.
 
 Classes:
-    Certificate: Represents a certificate entity.
-    CertificateFactory: Provides methods for creating certificates.
     Template: Manages HTML templates with associated CSS and assets.
 """
 
-from .certificate import Certificate
-from .certificate_factory import CertificateFactory
 from .template import Template
 
-__all__ = ['Certificate', 'CertificateFactory', 'Template']
+__all__ = ["Template"]
