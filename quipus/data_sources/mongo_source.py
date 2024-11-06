5f18d7594159afbd767c5892d7985a3bb75dcd2a
J1Loop <j1dev@proton.me>
2024-11-06 11:18:04 +0100

    [REF] Adding DBConfig & better exception management

--- quipus/data_sources/mongo_source.py
+++ quipus/data_sources/mongo_source.py
@@ -1,129 +1,138 @@
-from typing import Optional
+from typing import Optional, override
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
-from .database_source import DataBaseSource
+
+from quipus.utils import DBConfig
+from quipus.data_sources import DataBaseSource
 
 import polars as pl
 
 
 class MongoDBSource(DataBaseSource):
     """
-    Class for managing connections to a MongoDB database using pymongo.
+    A class for managing connections and data retrieval from a MongoDB
+    database using pymongo.
 
     Attributes:
-        connection_string: The connection string for the MongoDB database.
-        database_name: The name of the database to connect to.
-        collection_name: The name of the collection to query from.
-        query: The query to execute on the collection.
+        connection_string (str): The connection string for the MongoDB
+          database.
+        collection_name (str): The name of the collection to query.
+        query (dict): The query to be executed on the collection.
+
+    Methods:
+        initialize_pool(min_connections: int, max_connections: int) -> None:
+            Initializes the MongoDB client and sets up connection pooling.
+
+        connect() -> None:
+            Establishes a connection to the MongoDB database and sets the connected status.
+
+        disconnect() -> None:
+            Closes the MongoDB connection and sets the connection status to disconnected.
+
+        load_data() -> pl.DataFrame:
+            Loads data from the MongoDB collection and returns it as a Polars DataFrame.
+
+        get_columns(table_name: str) -> list[str]:
+            Retrieves a list of fields from the first document in the specified collection.
+
+        _build_connection_string(db_config: DBConfig, use_srv: bool) -> str:
+            Builds a MongoDB connection string based on the given configuration.
     """
 
     def __init__(
         self,
-        connection_string: str,
-        database_name: str,
         collection_name: str,
         query: Optional[dict] = {},
+        connection_string: Optional[str] = None,
+        db_config: Optional[DBConfig] = None,
+        use_srv: Optional[bool] = False,
     ):
-        super().__init__(connection_string)
-        self._client = None
-        self._database = None
-        self.database_name = database_name
+        """
+        Initializes a MongoDBSource instance with the specified parameters.
+
+        Parameters:
+            collection_name (str): The name of the collection to query.
+            query (Optional[dict], optional): The MongoDB query to execute. Defaults to an empty dictionary.
+            connection_string (Optional[str], optional): The connection string for the database.
+                Defaults to None, which constructs it from db_config if provided.
+            db_config (Optional[DBConfig], optional): A DBConfig instance for constructing the connection string.
+                Defaults to None.
+            use_srv (Optional[bool], optional): Whether to use the '+srv' scheme for MongoDB. Defaults to False.
+
+        Raises:
+            ValueError: If the collection_name is empty or invalid.
+        """
+        if db_config and not connection_string:
+            connection_string = self._build_connection_string(db_config, use_srv)
+
+        super().__init__(connection_string=connection_string, db_config=db_config)
         self.collection_name = collection_name
         self.query = query
+        self._client = None
+        self._database = None
 
     @property
     def query(self) -> dict:
+        """
+        dict: The MongoDB query to be executed.
+
+        Raises:
+            ValueError: If the query is not a dictionary.
+        """
         return self._query
 
     @query.setter
     def query(self, value: dict) -> None:
+        """
+        Sets the MongoDB query.
+
+        Parameters:
+            value (dict): The MongoDB query.
+
+        Raises:
+            ValueError: If the provided query is not a dictionary.
+        """
         if not isinstance(value, dict):
             raise ValueError("The query must be a dictionary.")
         self._query = value
 
     @property
-    def database_name(self) -> str:
-        return self._database_name
-
-    @database_name.setter
-    def database_name(self, value: str) -> None:
-        if not isinstance(value, str):
-            raise ValueError("The database name must be a string.")
-        if value.strip() == "":
-            raise ValueError("The database name cannot be empty.")
-        self._database_name = value
-
-    @property
     def collection_name(self) -> str:
+        """
+        str: The name of the collection to query.
+
+        Raises:
+            ValueError: If the collection name is not a string or is empty.
+        """
         return self._collection_name
 
     @collection_name.setter
     def collection_name(self, value: str) -> None:
-        if not isinstance(value, str):
-            raise ValueError("The collection name must be a string.")
-        if value.strip() == "":
-            raise ValueError("The collection name cannot be empty.")
+        """
+        Sets the collection name for the MongoDB query.
+
+        Parameters:
+            value (str): The name of the collection.
+
+        Raises:
+            ValueError: If the collection name is not a string or is empty.
+        """
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("The collection name must be a non-empty string.")
         self._collection_name = value
-
-    @classmethod
-    def build_connection_string(
-        cls,
-        host: str,
-        port: Optional[int] = None,
-        database: Optional[str] = None,
-        user: Optional[str] = None,
-        password: Optional[str] = None,
-        use_srv: bool = False,
-        options: Optional[dict[str, str]] = None,
-    ) -> str:
-        """
-        Builds a MongoDB connection string from the provided parameters.
-
-        Args:
-            host (str): The hostname or IP address of the MongoDB server.
-            port (int, optional): The port number to connect to (default for MongoDB is 27017).
-            database (str, optional): The name of the database to connect to.
-            user (str, optional): The username for authentication.
-            password (str, optional): The password for authentication.
-            use_srv (bool, optional): Whether to use the SRV protocol for clusters.
-            options (dict, optional): Additional connection options as key-value pairs.
-
-        Returns:
-            str: The connection string for the MongoDB database.
-
-        Example:
-            >>> connection_string = MongoDBSource.build_connection_string(
-            ...     user="admin", password="password", host="localhost", port=27017, database="mydatabase"
-            ... )
-            >>> print(connection_string) # mongodb://admin:password@localhost:27017/mydatabase
-
-            >>> connection_string = MongoDBSource.build_connection_string(
-            ...     host="cluster0-abcde.mongodb.net", user="admin", password="password", use_srv=True
-            ... )
-            >>> print(connection_string) # mongodb+srv://admin:password
-        """
-        protocol = "mongodb+srv" if use_srv else "mongodb"
-        credentials = f"{user}:{password}@" if user and password else ""
-        port_str = f":{port}" if port and not use_srv else ""
-        db_str = f"/{database}" if database else ""
-        options_str = ""
-
-        if options:
-            options_str = "?" + "&".join(
-                f"{key}={value}" for key, value in options.items()
-            )
-
-        return f"{protocol}://{credentials}{host}{port_str}{db_str}{options_str}"
 
     def initialize_pool(self, min_connections: int = 1, max_connections: int = 10):
         """
-        Initializes the connection pool for MongoDB.
+        Initializes the connection pool for MongoDB by creating a MongoClient instance.
 
-        MongoDB typically handles connection pooling automatically, so this method simply initializes the client.
+        MongoDB handles connection pooling automatically, so this method mainly initializes the client.
 
-        Args:
-            min_connections (int): The minimum number of connections in the pool.
-            max_connections (int): The maximum number of connections in the pool.
+        Parameters:
+            min_connections (int): The minimum number of connections in the pool. Defaults to 1.
+            max_connections (int): The maximum number of connections in the pool. Defaults to 10.
+
+        Raises:
+            ConnectionError: If the client fails to connect to MongoDB.
         """
         try:
             self._client = MongoClient(
@@ -132,77 +141,102 @@ class MongoDBSource(DataBaseSource):
                 maxPoolSize=max_connections,
             )
             self._client.admin.command("ping")
-            self._database = self._client[self.database_name]
+            self._database = self._client[self.db_config.database]
 
         except ConnectionFailure as e:
-            raise RuntimeError(f"Failed to connect to MongoDB: {e}")
+            raise ConnectionError(f"Failed to connect to MongoDB: {e}")
 
     def connect(self):
-        """Sets the connection status to True if the client is initialized."""
+        """
+        Establishes a connection to the MongoDB database and sets the connected status.
+
+        Raises:
+            ConnectionError: If an error occurs while trying to connect to the database.
+        """
+        if not self._client:
+            self.initialize_pool()
+
+        if self._database is None:
+            self._database = self._client[self.db_config.database]
+
         try:
-            if self._client is not None:
-                self._client.admin.command("ping")
-                if self._database is not None:
-                    self._connected = True
-                else:
-                    raise RuntimeError("Database instance not found.")
-            else:
-                raise RuntimeError(
-                    "MongoDB client not initialized. Call `initialize_pool()` first."
-                )
+            self._client.admin.command("ping")
+            self._connected = True
         except Exception as e:
             self._connected = False
-            raise RuntimeError("Failed to connect to the MongoDB database.") from e
+            raise ConnectionError("Failed to connect to the MongoDB database.") from e
 
     def disconnect(self):
-        """Closes the MongoDB client connection and sets the connection status to False."""
-        if self._client:
-            self._client.close()
-            self._is_connected = False
-        else:
+        """
+        Closes the MongoDB client connection and sets the connection status to False.
+
+        Raises:
+            RuntimeError: If the MongoDB client is not initialized.
+        """
+        if not self._client:
             raise RuntimeError("MongoDB client not initialized.")
+        self._client.close()
+        self._connected = False
 
     def load_data(self) -> pl.DataFrame:
         """
-        Loads data from a MongoDB collection.
+        Loads data from the specified MongoDB collection based on the query.
 
         Returns:
             pl.DataFrame: A Polars DataFrame containing the query results.
-        """
-        if not self._connected or self._client is None or self._database is None:
-            raise RuntimeError("Not connected to the MongoDB database.")
 
-        collection = self._database[self.collection_name]
-        result_cursor = collection.find(
-            self.query,
-        )
-
-        # if limit > 0:
-        #     result_cursor = result_cursor.limit(limit)
-
-        result = list(result_cursor)
-
-        if result:
-            return self.to_polars_df(result)
-        else:
-            return pl.DataFrame()
-
-    def get_columns(self) -> list[str]:
-        """
-        Retrieves the list of fields from the first document in the collection.
-
-        Returns:
-            list[str]: The list of fields in the collection.
+        Raises:
+            ConnectionError: If not connected to the database.
+            ValueError: If an error occurs during data loading.
         """
         if not self._connected or self._database is None:
-            raise RuntimeError("Not connected to the MongoDB database.")
+            raise ConnectionError("Not connected to the MongoDB database.")
 
         collection = self._database[self.collection_name]
+        result_cursor = collection.find(self.query)
+
+        result = list(result_cursor)
+        return self.to_polars_df(result) if result else pl.DataFrame()
+
+    @override
+    def get_columns(self, table_name: str, *args, **kwargs) -> list[str]:
+        """
+        Retrieves the list of fields from the first document in the specified MongoDB collection.
+
+        Parameters:
+            table_name (str): The name of the collection.
+
+        Returns:
+            list[str]: A list of field names in the collection.
+
+        Raises:
+            ConnectionError: If not connected to the database.
+            ValueError: If the collection is empty or does not exist.
+        """
+        if not self._connected or self._database is None:
+            raise ConnectionError("Not connected to the MongoDB database.")
+
+        collection = self._database[table_name]
         document = collection.find_one()
 
-        if document:
-            return list(document.keys())
+        if not document:
+            raise ValueError(f"Collection '{table_name}' is empty or does not exist.")
+
+        return list(document.keys())
+
+    def _build_connection_string(self, db_config: DBConfig, use_srv: bool) -> str:
+        """
+        Constructs a MongoDB connection string based on the provided configuration.
+
+        Parameters:
+            db_config (DBConfig): The database configuration object.
+            use_srv (bool): Whether to use the '+srv' scheme for MongoDB.
+
+        Returns:
+            str: The constructed connection string.
+        """
+        scheme = "mongodb+srv" if use_srv else "mongodb"
+        if use_srv:
+            return f"{scheme}://{db_config.user}:{db_config.password}@{db_config.host}/{db_config.database}"
         else:
-            raise RuntimeError(
-                f"Collection '{self.collection_name}' is empty or does not exist."
-            )
+            return f"{scheme}://{db_config.user}:{db_config.password}@{db_config.host}:{db_config.port}/{db_config.database}"
