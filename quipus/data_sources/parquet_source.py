5a6596f88384a368a0eb7b022d415994a53f2826
J1Loop <j1dev@proton.me>
2024-11-06 11:16:30 +0100

    [REF] Not overshadowing polars exceptions on ParquetSource

--- quipus/data_sources/parquet_source.py
+++ quipus/data_sources/parquet_source.py
@@ -1,16 +1,47 @@
 from pathlib import Path
 from typing import Any, Optional, Union
+
 import polars as pl
-from .file_source import FileSource
+
+from quipus.data_sources import FileSource
 
 
 class ParquetSource(FileSource):
+    """
+    A class for loading and processing data from Parquet files.
+
+    Inherits from:
+        FileSource: Base class for handling file sources.
+
+    Attributes:
+        file_path (Union[str, Path]): The path to the Parquet file.
+        columns (Optional[list[str]]): Specific columns to read from the file.
+        read_options (Optional[dict[str, Any]]): Additional options for reading the file.
+
+    Methods:
+        load_data() -> pl.DataFrame:
+            Loads data from the Parquet file into a Polars DataFrame.
+
+        get_columns() -> list[str]:
+            Retrieves the list of columns from the Parquet file.
+    """
+
     def __init__(
         self,
         file_path: Union[str, Path],
         columns: Optional[list[str]] = None,
         read_options: Optional[dict[str, Any]] = None,
     ):
+        """
+        Initializes a ParquetSource instance with the specified parameters.
+
+        Parameters:
+            file_path (Union[str, Path]): The path to the Parquet file.
+            columns (Optional[list[str]], optional): Columns to be read from the file.
+                Defaults to None, which reads all columns.
+            read_options (Optional[dict[str, Any]], optional): Additional options for reading
+                the file, passed directly to the Polars reader. Defaults to None.
+        """
         super().__init__(
             file_path=file_path,
             columns=columns,
@@ -20,32 +51,27 @@ class ParquetSource(FileSource):
     def load_data(self) -> pl.DataFrame:
         """
         Loads data from the Parquet file into a Polars DataFrame.
-        """
-        try:
-            df = pl.read_parquet(
-                source=self.file_path, columns=self.columns, **self.read_options
-            )
-            return df
-        except Exception as e:
-            raise RuntimeError(
-                f"An error occurred while loading data from {self.file_path}."
-            ) from e
 
-    def get_columns(self) -> list[str]:
+        Returns:
+            pl.DataFrame: A Polars DataFrame containing the data from the Parquet file.
+
+        Raises:
+            RuntimeError: If an error occurs while loading the data.
+        """
+        return pl.read_parquet(
+            source=self.file_path, columns=self.columns, **self.read_options
+        )
+
+    def get_columns(self, *args, **kwargs) -> list[str]:
         """
         Retrieves the list of columns from the Parquet file.
 
         Returns:
-            list[str]: A list of column names.
+            list[str]: A list of column names from the Parquet file.
         """
-        try:
-            df = pl.read_parquet(
-                source=self.file_path,
-                n_rows=0,
-                **self.read_options,
-            )
-            return df.columns
-        except Exception as e:
-            raise RuntimeError(
-                f"An error occurred while getting columns from {self.file_path}."
-            ) from e
+        df = pl.read_parquet(
+            source=self.file_path,
+            n_rows=0,
+            **self.read_options,
+        )
+        return df.columns
